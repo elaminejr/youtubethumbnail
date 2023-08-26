4a14ee2c8c626aad173b51bbd5f20f0445d99a97
elaminejr <126716987+elaminejr@users.noreply.github.com>
2023-08-26 18:51:43 +0100

    adsense

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,13 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Add your AdSense script here */}
+          <scriptY
+            data-ad-client="pub-6225203432253385"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
+          {/* Other head elements */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
